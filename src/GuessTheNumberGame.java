9e9ea9ad125df5293a795bd37a738e4bc1b3704d
vanelopezr <vanne.lopez.r@gmail.com>
2024-01-31 13:06:00 -0300

    Final

--- src/GuessTheNumberGame.java
+++ src/GuessTheNumberGame.java
@@ -2,17 +2,17 @@
 import java.util.Random;
 
 public class GuessTheNumberGame {
-    public static int targetNumber;
 
+// se genera el numero random y lo devuelve.
     public static int generateRandomNumber() {
         Random random = new Random();
-        targetNumber = random.nextInt(100) + 1;
-        return targetNumber;
+        return random.nextInt(1, 100) + 1;
     }
 
+//revisamos la suposiscion del jugador y la comparamos con el targetNumber
     public static boolean checkGuess(Player player, int targetNumber) {
+//se obtiene la suposicion de jugador
         int guess = player.makeGuess();
-        if (player instanceof HumanPlayer) {
             if (guess < targetNumber) {
                 System.out.println("Too Low!");
             } else if (guess > targetNumber) {
@@ -23,26 +23,16 @@ public class GuessTheNumberGame {
 
             }
             return false;
-        } else if (player instanceof ComputerPlayer) {
-            if (guess < targetNumber) {
-                System.out.println("Too Low!");
-            } else if (guess > targetNumber) {
-                System.out.println("Too High!");
-
-            } else {
-                return true;
-            }
-        }
-
-        return false;
     }
     public static void main(String[] args) {
-        System.out.println("Target Number " + generateRandomNumber());
+        int targetNumber = generateRandomNumber();
+        //System.out.println("Target Number " + targetNumber);
         System.out.println("Welcome to Guess the Number Game!");
 
+        //creamos las instancias de los jugadores.
         Player humanPlayer = new HumanPlayer();
         Player computerPlayer = new ComputerPlayer();
-
+         //turnos
         do{
             System.out.println("----- Player 1 -----");
             if (checkGuess(humanPlayer, targetNumber)){
@@ -53,7 +43,7 @@ public class GuessTheNumberGame {
             }
             System.out.println("----- Computer Player -----");
             if (checkGuess(computerPlayer, targetNumber)) {
-                System.out.println("Computer Player guessed the number in " +computerPlayer.getGuesses().size() + " attempts.");
+                System.out.println("Computer Player guessed the number in " + computerPlayer.getGuesses().size() + " attempts.");
                 System.out.println("Attempts: " + computerPlayer.getGuesses().toString());
 
                 break;
